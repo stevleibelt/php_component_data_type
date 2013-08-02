3e6ab2971b4f6bb9f27a4d9a93ad408edffe41c2
stevleibelt <stev@leibelt.de>
2013-08-03 01:01:31 +0200

    scribbeld string

---
+++ source/Net/Bazzline/Component/DataType/String.php
@@ -0,0 +1,18 @@
+<?php
+
+class String extends DataTypeAbstract
+{
+
+    public function contains($string)
+    {
+    }
+    public function startsWith($prefix)
+    {
+    }
+    public function endsWith($suffix)
+    {
+    }
+    public function __toString()
+    {
+    }
+}
