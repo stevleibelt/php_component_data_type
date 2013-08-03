edd7d9adfaabc51c6e8c29edc0b857ec345f27cd
stevleibelt <stev@leibelt.de>
2013-08-03 20:58:51 +0200

    Added testcase

---
+++ test/Net/Bazzline/Component/DataType/TestCase.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-03 
+ */
+
+namespace Net\Bazzline\Component\DataType;
+
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Class TestCase
+ *
+ * @package Net\Bazzline\Component\DataType
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-03
+ */
+class TestCase extends  PHPUnit_Framework_TestCase{}
