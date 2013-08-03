26ecd217ab6fcac93d6c42acbb2e8502a349e487
stevleibelt <stev@leibelt.de>
2013-08-03 20:12:44 +0200

    added doc

--- source/Net/Bazzline/Component/DataType/DataTypeAbstract.php
+++ source/Net/Bazzline/Component/DataType/DataTypeAbstract.php
@@ -1,23 +1,62 @@
 <?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-03
+ */
 
+namespace Net\Bazzline\Component\DataType;
+
+/**
+ * Class DataTypeAbstract
+ *
+ * @package Net\Bazzline\Component\DataType
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-03
+ */
 abstract class DataTypeAbstract
 {
+    /**
+     * @var mixed
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-03
+     */
     protected $value;
 
+    /**
+     * @param mixed $value
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-03
+     */
     public function set($value)
     {
-        //validator?
-        $this->value = $value;
+        $this->value = $this->castToType($value);
 
         return $this;
     }
 
+    /**
+     * @return mixed
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-03
+     */
     public function get()
     {
         return $this->value;
     }
 
+    /**
+     * @return string
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-03
+     */
     abstract public function __toString();
 
+    /**
+     * @param mixed $value
+     * @return mixed
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-03
+     */
     abstract protected function castToType($value);
 }
