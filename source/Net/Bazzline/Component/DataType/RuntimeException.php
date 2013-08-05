ae4cc83f1b23d8aa50e765d20a928b40e659bace
stevleibelt <stev@leibelt.de>
2013-08-05 20:47:31 +0200

    Added exception

---
+++ source/Net/Bazzline/Component/DataType/RuntimeException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-05 
+ */
+
+namespace Net\Bazzline\Component\DataType;
+
+use RuntimeException as ParentClass;
+
+/**
+ * Class RuntimeException
+ *
+ * @package Net\Bazzline\Component\DataType
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-05
+ */
+class RuntimeException extends ParentClass {}
