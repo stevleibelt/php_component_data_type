63608ec8e5c2fc08978ea7f702e6cc1a8d55b57a
stevleibelt <stev@leibelt.de>
2013-08-07 22:46:47 +0200

    Created ArrayAbleInterface

---
+++ source/Net/Bazzline/Component/DataType/ArrayableInterface.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-07 
+ */
+
+namespace Net\Bazzline\Component\DataType;
+
+use ArrayAccess;
+use Countable;
+use Iterator;
+
+/**
+ * Class ArrayableInterface
+ *
+ * @package Net\Bazzline\Component\Utility
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-07
+ */
+interface ArrayableInterface extends ArrayAccess, Countable, Iterator
+{
+    /**
+     * @param array $array
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-07
+     */
+    public function fromArray(array $array);
+
+    /**
+     * @return array
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-07
+     */
+    public function toArray();
+}
