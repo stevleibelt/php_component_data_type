cec6a94bb25f77a9b824985e239e74adadf061d8
stevleibelt <stev@leibelt.de>
2013-09-02 00:03:15 +0200

    added plain merge with complex

--- test/Test/Net/Bazzline/Component/DataType/DataArrayTest.php
+++ test/Test/Net/Bazzline/Component/DataType/DataArrayTest.php
@@ -25,7 +25,7 @@ class DataArrayTest extends TestCase
     public static function mergeTestCaseDataProvider()
     {
         return array(
-            'merge plain array with plain array' => array(
+            'plain array with plain array' => array(
                 'origin' => new DataArray(array(
                     'foo' => 'foo',
                     'bar' => 'foo'
@@ -40,7 +40,7 @@ class DataArrayTest extends TestCase
                 )),
                 'overwrite' => true
             ),
-            'merge plain array with plain array and overwrite off' => array(
+            'plain array with plain array and overwrite off' => array(
                 'origin' => new DataArray(array(
                     'foo' => 'bar',
                     'bar' => 'foo'
@@ -49,12 +49,44 @@ class DataArrayTest extends TestCase
                     'bar' => 'bar',
                     'foobar' => 'barfoo'
                 )),
-                'exptected' => new DataArray(array(
+                'expected' => new DataArray(array(
                     'foo' => 'bar',
                     'bar' => 'foo',
                     'foobar' => 'barfoo'
                 )),
                 'overwrite' => false
+            ),
+            'plain array with complex array' => array(
+                'origin' => new DataArray(array(
+                    'foo' => 'bar',
+                    'complex' => 'plain'
+                )),
+                'merge' => new DataArray(array(
+                    'complex' => array(
+                        3 => 'int',
+                        'string' => 'string',
+                        'array' => array(
+                            'foo' => 'bar'
+                        ),
+                        'DataArray' => new DataArray(array(
+                            'the message' => 'is love'
+                        ))
+                    )
+                )),
+                'expected' => new DataArray(array(
+                    'foo' => 'bar',
+                    'complex' => array(
+                        3 => 'int',
+                        'string' => 'string',
+                        'array' => array(
+                            'foo' => 'bar'
+                        ),
+                        'DataArray' => new DataArray(array(
+                            'the message' => 'is love'
+                        ))
+                    )
+                )),
+                'overwrite' => true
             )
         );
     }
